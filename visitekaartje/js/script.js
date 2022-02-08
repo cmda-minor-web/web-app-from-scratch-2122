63886e2494e7c5a35585a8d192948f206cbe62c9
jornveltrop <jorn1veltrop@gmail.com>
2022-02-08 12:27:22 +0100

    Progressive enhancement fix

--- visitekaartje/js/script.js
+++ visitekaartje/js/script.js
@@ -2,9 +2,11 @@ const meerButton = document.querySelector("#meerButton");
 const header = document.querySelector("header");
 const nav = document.querySelector("nav");
 const h1Name = document.querySelector("h1");
+const main = document.querySelector("main");
 
 //Progressive disclosure
 nav.classList.toggle("hiddenTransform");
+main.classList.toggle("overflowHidden");
 
 function overgangInfo() {
     header.classList.toggle("headerKlein");
