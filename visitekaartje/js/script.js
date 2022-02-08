caf7bc1a0795f9e2e391f05e653cae929da55758
jornveltrop <jorn1veltrop@gmail.com>
2022-02-08 11:22:01 +0100

    techniek versie 1

--- visitekaartje/js/script.js
+++ visitekaartje/js/script.js
@@ -1,12 +1,22 @@
 const meerButton = document.querySelector("#meerButton");
 const header = document.querySelector("header");
+const nav = document.querySelector("nav");
+const h1Name = document.querySelector("h1");
 
+//Progressive disclosure
+nav.classList.toggle("hiddenTransform");
 
 function overgangInfo() {
-    console.log("test");
     header.classList.toggle("headerKlein");
-}
+    nav.classList.toggle("hiddenTransform");
+    h1Name.classList.toggle("hiddenTransformFade");
 
+    if (meerButton.innerHTML.length == 1) {
+        meerButton.innerHTML = "Meer info";
+    } else {
+        meerButton.innerHTML = "X";
+    }
+}
 
 meerButton.addEventListener("click", overgangInfo);
 
