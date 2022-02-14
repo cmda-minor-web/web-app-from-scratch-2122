30a754a907f63f73afd991e76ed1241cc29abdad
AronPelgrim <aron.pelgrim@hva.nl>
2022-02-14 20:02:48 +0100

    fetch json data

---
+++ scripts/mainFunctions.js
@@ -0,0 +1,23 @@
+const url = 'https://www.rijksmuseum.nl/api/nl/collection?key=S0VK6DCj&involvedMaker=Rembrandt+van+Rijn'
+
+getPaintings()
+
+function getPaintings() {
+    
+    const bodyList = bodyE('ul')
+
+    fetch(url) 
+        .then(function(response){
+            return response.json() 
+        })
+        .then(function(paintings){   
+            for (let i = 0; i < paintings.artObjects.length; i++) {     
+            bodyList.insertAdjacentHTML('afterbegin', 
+            `<li><img src="${paintings.artObjects[i].webImage.url}"></li>`) 
+            }
+        })
+}
+
+function bodyE(element) {
+    return document.querySelector(element)
+}
