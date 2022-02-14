b39066e88ec56c1628c7d03cf6237ead5a40ca7b
AronPelgrim <74137185+AronPelgrim@users.noreply.github.com>
2022-02-14 16:16:52 +0100

    Delete examples directory

--- examples/modules/static/js/modules/loader.js
+++
@@ -1,6 +0,0 @@
-export function loader(state) {
-  const loader = document.querySelector('div.loader');
-  state === 'active'
-    ? loader.classList.add('loading')
-    : loader.classList.remove('loading');
-}
