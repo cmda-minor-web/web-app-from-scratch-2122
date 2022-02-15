a894b058eb80d88b29395cd61a086546454474fe
ppijn <pepijnjong2000@gmail.com>
2022-02-15 16:16:49 +0100

    RIjksmuseum a little step further

--- spa/js/script.js
+++ spa/js/script.js
@@ -6,6 +6,8 @@
 // const rijks ="https://www.rijksmuseum.nl/api/nl/collection/SK-C-5?key=8Rynz75W";
 const rijksAPI =
   "https://www.rijksmuseum.nl/api/nl/collection?key=8Rynz75W&p=0-n&ps=10&imgonly=true";
+
+const stateDisplay = document.getElementsByClassName("emptyList");
 // 2. the Story
 
 getData();
@@ -13,6 +15,8 @@ getData();
 // 3. Functions
 
 function getData() {
+  // stateDisplay.textContent = "Loading";
+
   fetch(rijksAPI)
     .then(function (response) {
       return response.json();
@@ -20,14 +24,30 @@ function getData() {
     .then(function (collection) {
       console.log(collection);
       const list = $("ul");
+      const skeleton = $("ul.empty-list");
+      console.log(skeleton);
+      skeleton.classList.toggle("hidden");
       for (let i = 0; i < collection.artObjects.length; i++) {
-        list.insertAdjacentHTML(
-          "beforebegin",
-          `<li>
-          <img class="art" src="${collection.artObjects[i].webImage.url}" alt="${collection.artObjects[i].title}"/>
-          <h1>${collection.artObjects[i].title}</h1>
-          </li>`
-        );
+        fetch(
+          "https://www.rijksmuseum.nl/api/nl/collection/" +
+            collection.artObjects[i].objectNumber +
+            "?key=8Rynz75W&p=0-n&ps=10&imgonly=true"
+        )
+          .then(function (response) {
+            return response.json();
+          })
+          .then(function (detailed) {
+            list.insertAdjacentHTML(
+              "afterbegin",
+              `<li>
+              <img class="art" src="${
+                detailed.artObject.webImage.url.slice(0, -3) + "=s1000"
+              }" alt="${detailed.artObject.title}"/>
+              <h1>${detailed.artObject.title}</h1>
+              <h1>${detailed.artObject.dating.presentingDate}</h1>
+              </li>`
+            );
+          });
       }
     });
 
