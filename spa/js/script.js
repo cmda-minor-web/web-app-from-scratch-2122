0d3d6b7f1500a29ea7eda300da5bf26ad19202f4
jornveltrop <jorn1veltrop@gmail.com>
2022-02-18 12:39:32 +0100

    Nette JS

--- spa/js/script.js
+++ spa/js/script.js
@@ -1,69 +1,53 @@
-
-const sectionData = document.querySelector('section');
-let inputField = 'bloem'
-
-function setData () {
-    const articleElements = document.querySelectorAll("section > article");
-    const article = Array.prototype.slice.call(articleElements);
-
-    for (var i = 0, l = articleElements.length; i < l; i++) {
-        article[i].remove();
-    }
-    
-    const getURL = 'https://www.rijksmuseum.nl/api/nl/collection?key=AbH3UnTw&q=' + inputField;
-    console.log(getURL)
-
-
-    
-    fetch(getURL).then(response => response.json())
-    .then(data => {
-        const obj = data.artObjects;
-        console.log(obj);
-        obj.forEach(number => {
-            console.log(number.id);    
-            sectionData.insertAdjacentHTML('beforeend',`
-                
-                <article>
-                    <h2>${number.title}</h2>
-                    <img src="${number.webImage.url}">
-                </article
-            `)
-        });
-    }).catch(error => console.log(error))
-}
-
-setData();
+const sectionData = document.querySelector("section");
+const aantalResults = 100;
+const imgSize = 700;
 
 function getAndRenderData () {
+    
+    //Verwijder huidige kunstwerken
     const articleElements = document.querySelectorAll("section > article");
-    const article = Array.prototype.slice.call(articleElements);
-    const h1 = document.querySelector("h1");
-
     for (var i = 0, l = articleElements.length; i < l; i++) {
-        article[i].remove();
+        articleElements[i].remove();
     }
     
-    let inputField = document.querySelector('input').value;
+    //Titel vullen met zoekterm
+    const h1 = document.querySelector("h1");
+    let inputField = document.querySelector("input").value;
     h1.innerHTML = "Gezocht naar: " + inputField; 
-    const getURL = 'https://www.rijksmuseum.nl/api/nl/collection?key=AbH3UnTw&q=' + inputField;
-    console.log(getURL)
 
+    //In geval geen input zoekbalk, laat uitgelicht zien
+    if (inputField == 0) {
+        inputField = "";
+        h1.innerHTML = "Uitgelicht"; 
+    }
 
+    //Stel URL in (100)
+    const getURL = "https://www.rijksmuseum.nl/api/nl/collection?key=AbH3UnTw&q=" + inputField + "&ps=" + aantalResults;
     
+    //Haal data op
     fetch(getURL).then(response => response.json())
     .then(data => {
-        const obj = data.artObjects;
-        console.log(obj);
-        obj.forEach(number => {
-            console.log(number.id);    
-            sectionData.insertAdjacentHTML('beforeend',`
-                
-                <article>
-                    <h2>${number.title}</h2>
-                    <img src="${number.webImage.url}">
-                </article
-            `)
-        });
+        
+        //Pak kunst objecten
+        const objects = data.artObjects;
+        console.log(objects);
+
+        if (objects.length == 0) {
+
+        } 
+        else {
+            //Genereer HTML blokken voor elk object
+            objects.forEach(number => {
+                let scaledImg = number.webImage.url + imgSize;
+                sectionData.insertAdjacentHTML("beforeend",`
+                    <article>
+                        <h2>${number.title}</h2>
+                        <img src="${scaledImg}">
+                    </article
+                `)
+            });
+        }
     }).catch(error => console.log(error))
 }
 
+getAndRenderData();
