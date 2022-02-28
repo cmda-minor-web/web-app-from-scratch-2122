eeeb2273233bb2ddf6c751c0070d1a0f7bedb8c9
ppijn <pepijnjong2000@gmail.com>
2022-02-28 15:28:00 +0100

    dag 5

--- spa/js/main.js
+++ spa/js/main.js
@@ -1,3 +1,5 @@
 import { getData } from "./modules/getdata.js";
+import { jaar } from "./modules/jaar.js";
 
 getData();
+jaar();
---
+++ spa/js/modules/jaar.js
@@ -0,0 +1,21 @@
+export function jaar() {
+  const jaarButton = $(".jaar-button");
+  const jaarInvoer = $(".jaar-open");
+  const form = $("form");
+
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+  });
+
+  jaarButton.addEventListener("click", function (e) {
+    console.log(e);
+    e.preventDefault();
+    jaarButton.classList.toggle("hidden");
+    jaarInvoer.classList.toggle("hidden");
+    console.log(jaarButton);
+  });
+
+  function $(element) {
+    return document.querySelector(element);
+  }
+}
