dd10a3e5c36fd09a370b349bb58c822e4c8b6a87
ppijn <pepijnjong2000@gmail.com>
2022-03-01 15:07:02 +0100

    laatste commit week 3 dag 2

--- spa/js/main.js
+++ spa/js/main.js
@@ -2,10 +2,8 @@ import { getData } from "./modules/getdata.js";
 import { jaar } from "./modules/jaar.js";
 import { search } from "./modules/search.js";
 import { page } from "./modules/page.js";
-// import { $ } from "./modules/dqselector.js";
 
 getData();
 jaar();
 search();
 page();
-// $();
