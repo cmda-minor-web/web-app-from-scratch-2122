5b7099fd1661f20dcb46f818a8dc1422c1fb0130
ppijn <pepijnjong2000@gmail.com>
2022-03-04 13:06:08 +0100

    Deleted stuff

--- examples/local-storage/assets/js/modules/config.js
+++
@@ -1,7 +0,0 @@
-const config = {
-    cors: 'https://cors-anywhere.herokuapp.com/',
-    base: 'https://api.darksky.net/forecast',
-    key: '/36a3855fd0934dd8210f55f62b4a207a'
-}
-
-export {config}
